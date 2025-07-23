d4009d066c5a13b20be67c0c51e53d2ccd9d662a
Matvey Fedorov <mfgolden@yandex.ru>
2025-07-23 20:27:12 +0300

    add configuration management with .env support

---
+++ config/config.go
@@ -0,0 +1,36 @@
+package config
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ENV  string
+	Port string
+
+	DatabaseURL    string
+	InternalAPIKey string
+}
+
+func LoadConfig() Config {
+	if os.Getenv("ENV") == "production" {
+		if err := godotenv.Load(".env"); err != nil {
+			panic("Error loading .env file")
+		}
+	}
+
+	port := os.Getenv("ENV")
+	if port == "" {
+		port = "8080"
+	}
+
+	return Config{
+		ENV:  os.Getenv("ENV"),
+		Port: port,
+
+		DatabaseURL:    os.Getenv("DATABASE_URL"),
+		InternalAPIKey: os.Getenv("INTERNAL_API_KEY"),
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MatTwix/RE-minder-Bots
 
 go 1.23.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
