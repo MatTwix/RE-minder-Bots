eb1b54a88e8f8f232fdc4908d0788065520522be
Matvey Fedorov <mfgolden@yandex.ru>
2025-07-26 14:33:16 +0300

    feat: enhance RabbitMQ consumer to handle notification tasks and retrieve user notification settings

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -1,11 +1,31 @@
 package consumer
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 
+	"github.com/MatTwix/RE-minder-Bots/bot"
 	"github.com/MatTwix/RE-minder-Bots/config"
+	"github.com/MatTwix/RE-minder-Bots/services"
 	"github.com/rabbitmq/amqp091-go"
 )
+
+type NotificationTask struct {
+	UserID  int    `json:"user_id"`
+	Message string `json:"message"`
+}
+
+type NotificationSettings struct {
+	ID                   int  `json:"id"`
+	UserID               int  `json:"user_id"`
+	TelegramNotification bool `json:"telegram_notification"`
+	DiscordNotification  bool `json:"discord_notification"`
+	VKNotification       bool `json:"vk_notification"`
+}
 
 func Start() {
 	cfg := config.LoadConfig()
@@ -50,7 +70,64 @@ func Start() {
 	go func() {
 		log.Println("RabbitMQ Consumer started. Waiting gor messages...")
 		for d := range msgs {
-			log.Printf("Received a message: %v", d.Body)
+			var task NotificationTask
+
+			err := json.Unmarshal(d.Body, &task)
+			if err != nil {
+				log.Printf("Error unmarshalling task: %v. Discarding message.", err)
+				d.Nack(false, false)
+				continue
+			}
+
+			log.Printf("Successfully parsed task for user_id: %d", task.UserID)
+
+			var settings NotificationSettings
+			url := fmt.Sprintf("%s/internal/notifications_settings/user/%d", cfg.MainAppUrl, task.UserID)
+			req, _ := http.NewRequest("GET", url, nil)
+			req.Header.Set("X-API-Key", cfg.InternalAPIKey)
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil || resp.StatusCode != http.StatusOK {
+				bodyBites, _ := io.ReadAll(resp.Body)
+				log.Printf("Error getting notifications settings for user %d: %v, StatusCode: %d, Body: %s", task.UserID, err, resp.StatusCode, string(bodyBites))
+				d.Nack(false, true)
+				continue
+			}
+			json.NewDecoder(resp.Body).Decode(&settings)
+			resp.Body.Close()
+
+			chats, err := services.GetChats(context.Background(), services.Condition{
+				Field:    "reminder_user_id",
+				Operator: services.Equal,
+				Value:    task.UserID,
+			})
+
+			if err != nil {
+				log.Printf("Error getting user %d chats: %v", task.UserID, err)
+				d.Nack(false, true)
+				continue
+			}
+
+			for _, chat := range chats {
+				platform := chat.Platform
+
+				botInstance, ok := bot.GetBot(platform)
+				if !ok {
+					log.Printf("No bot registered for platform: %s", platform)
+					continue
+				}
+
+				shouldSend := platform == "discord" && settings.DiscordNotification || platform == "vk" && settings.TelegramNotification || platform == "telegram" && settings.TelegramNotification
+
+				log.Printf("Send: %v", shouldSend)
+				if shouldSend {
+					err := botInstance.SendMessage(chat.ChatID, task.Message)
+					if err != nil {
+						log.Printf("Error sending message via %s for user %d: %v", platform, task.UserID, err)
+					}
+				}
+			}
+			log.Printf("Notifications for user %d sended!", task.UserID)
 
 			d.Ack(false)
 		}
--- models/Chat.go
+++ models/Chat.go
@@ -1,10 +1,12 @@
 package models
 
+import "time"
+
 type Chat struct {
-	ID             int64  `json:"id"`
-	ReminderUserID int64  `json:"reminder_user_id"`
-	Platform       string `json:"platform"`
-	ChatID         int64  `json:"chat_id"`
-	CreatedAt      string `json:"created_at"`
-	UpdatedAt      string `json:"updated_at"`
+	ID             int64     `json:"id"`
+	ReminderUserID int64     `json:"reminder_user_id"`
+	Platform       string    `json:"platform"`
+	ChatID         int64     `json:"chat_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
